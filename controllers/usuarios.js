3c16a10e7739c5b4bd195765cee8a67d3d28fadb
Nancy <nancycarrillo1224@gmail.com>
2022-04-28 22:39:36 -0500

    middlewares

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,72 +1,81 @@
-const { response }= require('express');
+const { response } = require('express');
 const bcryptjs = require('bcryptjs');
-const Usuario = require('../models/usuario')
+const Usuario = require('../models/usuario');
 
 
-const usuariosGet = (req, res = response)=> {
+const usuariosGet = (req, res = response) => {
 
-    const {q, nombre = 'no name', apikey } = req.query;
+    const { q, nombre = 'no name', apikey } = req.query;
 
     res.json({
-        msg:'get API - controladorGet',
+        msg: 'get API - controladorGet',
         q,
         nombre,
         apikey
     })
 }
 
-const usuariosPost =  async (req, res = response)=> {
+const usuariosPost = async (req, res = response) => {
+    // crear midleware personalizado 
 
-    const  {nombre, password, correo, rol} = req.body;
+   
+
+    const { nombre, password, correo, rol } = req.body;
     // verifica correo
-    const verificaEmail = Usuario.findOne({ correo });
-    if (verificaEmail){
+    const verificaEmail = await Usuario.findOne({ correo });
+
+    if (verificaEmail) {
         return res.status(400).json({
-            msg:'El correo ya existe '
+            msg: 'El correo ya existe '
         });
     }
     const salt = bcryptjs.genSaltSync();
 
-    const data ={
+    const data = {
         nombre,
         password,
         correo,
         rol
     }
-    data.password = bcryptjs.hashSync(password, salt );
-    const usuario = new Usuario( data );
+    data.password = bcryptjs.hashSync(password, salt);
+    const usuario = new Usuario(data);
+    try {
+        await usuario.save();
+        res.status(200).json({
+            data,
+            usuario,
+            msg: 'post API - controladorPost',
 
-    await usuario.save();
-    res.status(200).json({
-        data,
-        usuario,
-        msg:'post API - controladorPost',
-       
-    })
+        })
+    } catch (error) {
+        res.status(500).json({msg:'Error al guardar usuario',error});
+
+    }
+
 }
 
-const usuariosPut = (req, res = response)=> {
+const usuariosPut = (req, res = response) => {
     const id = req.params.id;
     res.json({
-        msg:'put API -controladorPut',
+        msg: 'put API -controladorPut',
         id
     })
 }
 
-const usuariosDelete =  (req, res = response )=> {
+const usuariosDelete = (req, res = response) => {
     res.json({
-        msg:'delete API -controladorDelete'
+        msg: 'delete API -controladorDelete'
     })
 
 }
 
-const usuariosPatch =  (req, res = response )=> {
+const usuariosPatch = (req, res = response) => {
     res.json({
-        msg:'patch API -controladorPatch'
+        msg: 'patch API -controladorPatch'
     })
 
 }
-module.exports={
+module.exports = {
     usuariosGet,
     usuariosPut,
     usuariosPost,
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,5 +1,8 @@
 
 const{ Router } = require('express');
+const { check } = require('express-validator');
+const Role = require('../models/role');
+const {validarCampos} = require('../middlewares/validar-campos');
 const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require('../controllers/usuarios');
 
 const router = Router();
@@ -9,7 +12,19 @@ router.get('/', usuariosGet );
 
 router.put('/:id',usuariosPut  );
 
-router.post('/', usuariosPost );
+router.post('/',[
+    check('nombre','El nombre es obligatorio').not().isEmpty(),
+    check('password','El password debe ser mayor a seis letras').isLength({min:6}),
+    // check('rol','No es un rol permitido').isIn(['ADMIN_ROLE','USER_ROLE']),
+    check('rol').custom( async  rol=>{
+        const existeRol = await Role.findOne({rol}) ;
+        if(!existeRol){
+            throw new Error(`Èl rol ${rol} no existe en la BD`)
+        }   
+    }),
+    check('correo','El correo no es valido').isEmail(),
+    validarCampos
+], usuariosPost );
 
 router.delete('/', usuariosDelete );
 
